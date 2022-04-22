ba3b59346f3d867d935274416aeb9ec9506b5617
Bhaumik-Tandan <bhaumik.tandan@gmail.com>
2022-04-22 11:40:34 +0530

    Table created

--- dataInsertion/index.php
+++ dataInsertion/index.php
@@ -1,4 +1,14 @@
 <?php
+include "./admin.php";
+include "./comments.php";
+include "./couriers.php";
+include "./customer.php";
+include "./login.php";
+include "./manage.php";
+include "./offices.php";
+include "./shipments.php";
+include "./tracks.php";
+include "../utils/executeQuery.php";
 function insertRecords($records)
 {
     foreach ($records as $record) {
@@ -7,3 +17,23 @@ function insertRecords($records)
 }
 
 ?>
+
+<?php
+if(isset($_POST['insertData'])) {
+    insertRecords($login);
+    insertRecords($offices);
+    insertRecords($comments);
+    insertRecords($customer);
+    insertRecords($shipments);
+    insertRecords($admin);
+    insertRecords($couriers);
+    insertRecords($manage);
+    insertRecords($tracks);
+    echo "Tables created successfully";
+}
+?>
+
+<form method="post">
+        <input type="submit" name="insertData" value="Insert Data"/>
+          
+</form>
--- index.php
+++ index.php
@@ -1,5 +1,21 @@
-Login ID:
-<input type="text" name="loginID" value="<?php echo $loginID; ?>"/>
+<?php
+include "./utils/db.php";
+if(isset($_POST['login'])) {
+    $loginID= $_POST['loginID'];
+    $password = $_POST['password'];
+    $sql = "SELECT * FROM users WHERE loginID = '$loginID' AND password = '$password'";
+    $conn=
+    $result = $conn->query($sql);
+
+
+}
+?>
+
+<form method="post">
+LOGIN ID:
+<input type="text" name="loginID" />
 <br/>
 Password:
-<input type="password" name="password" value="<?php echo $password; ?>"/>
+<input type="password" name="password"/>
+<button type="submit" name="login">Login</button>
+</form>
